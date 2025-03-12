1547b8e88137a97ab581eeaf4ff843ab298a3b9c
David Amorim <davidfilipeamorim@gmail.com>
2025-03-12 21:20:40 +0000

    Add more exceptions to exception handler

--- src/main/java/com/github/pi_tracking/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/pi_tracking/backend/exception/GlobalExceptionHandler.java
@@ -1,13 +1,20 @@
 package com.github.pi_tracking.backend.exception;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+import java.security.SignatureException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +48,44 @@ public class GlobalExceptionHandler {
         response.put("message", "Data validation error");
 
         return response;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    public Map<String, Object> handleBadCredentialsException(BadCredentialsException ex) {
+        return createErrorResponse("The username or password is incorrect");
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccountStatusException.class)
+    public Map<String, Object> handleAccountStatusException(AccountStatusException ex) {
+        return createErrorResponse("The account is disabled");
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public Map<String, Object> handleAccessDeniedException(AccessDeniedException ex) {
+        return createErrorResponse("You are not authorized to access this resource");
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(SignatureException.class)
+    public Map<String, Object> handleSignatureException(SignatureException ex) {
+        return createErrorResponse("The JWT signature is invalid");
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(ExpiredJwtException.class)
+    public Map<String, Object> handleExpiredJwtException(ExpiredJwtException ex) {
+        return createErrorResponse("The JWT token has expired");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleSecurityException(Exception exception) {
+        ProblemDetail errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+        errorDetail.setProperty("description", "Unknown internal server error.");
+
+        return errorDetail;
     }
 
     private Map<String, Object> createErrorResponse(String message) {
