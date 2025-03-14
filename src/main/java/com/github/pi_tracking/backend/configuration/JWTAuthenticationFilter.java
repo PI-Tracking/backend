842ee45af7b4b12cc87c0310cac56571a6a30e12
Francisca Silva <franciscafpsilva@ua.pt>
2025-03-14 18:40:47 +0000

    Implement using only cookies

--- src/main/java/com/github/pi_tracking/backend/configuration/JWTAuthenticationFilter.java
+++ src/main/java/com/github/pi_tracking/backend/configuration/JWTAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.github.pi_tracking.backend.service.JWTService;
 import io.micrometer.common.lang.NonNull;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.util.WebUtils;
 
 import java.io.IOException;
 
@@ -39,15 +41,15 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter{
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        Cookie cookie = WebUtils.getCookie(request, "accessToken");
 
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        if (cookie == null) {
             filterChain.doFilter(request, response);
             return;
         }
 
         try {
-            final String jwt = authHeader.substring(7);
+            final String jwt =cookie.getValue();
             final String userEmail = jwtService.extractUsername(jwt);
 
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
